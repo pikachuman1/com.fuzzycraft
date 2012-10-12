5415c33ddfdd686e3e10b535b29afd6b86f205be
MissingScrews <af732@drexel.edu>
2012-10-12 19:37:26 -0400

    Implemented Listener

--- ArenaManagement/src/com/fuzzycraft/ArenaManagement.java
+++ ArenaManagement/src/com/fuzzycraft/ArenaManagement.java
@@ -1,26 +1,36 @@
 package com.fuzzycraft;
 
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ArenaManagement extends JavaPlugin {
-	 
+
+	private PlayerListener pl = new PlayerListener(this);
+	
+	@Override
 	public void onEnable() {
 		getLogger().info("onEnable has been invoked!");
-	}
- 
-	public void onDisable() {
-		getLogger().info("onDisable has been invoked!");
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this.pl, this);
 	}
 	
-	public void onPlayerClick(PlayerInteractEvent evt) {
-		evt.getPlayer().sendMessage(evt.getClickedBlock().getLocation().toString());
-		
+	@Override
+	public void onDisable() {
+		getLogger().info("onDisable has been invoked!");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -37,4 +47,5 @@ public class ArenaManagement extends JavaPlugin {
 		}
 		return false;
 	}
+	
 }
--- ArenaManagement/src/com/fuzzycraft/GitTest.java
+++
@@ -1,7 +0,0 @@
-package com.fuzzycraft;
-
-public class GitTest {
-	public static void main(String[] args) {
-	    System.out.println("Git is fun");
-	}
-}
